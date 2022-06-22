c7dafb925dd9974a3c878f0bab64b1522de21cf4
EshginCeferli <eshginij@code.edu.az>
2022-06-23 02:19:54 +0400

    Class.Object

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+
+
+namespace ClassObject
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //int[] numbers = new int[8];
+
+        //numbers[0] = 4;
+        //numbers[1] = 8;
+        //numbers[2] = 55;
+
+
+        //int[] nums = {1, 4, 3, 5, 6, 10, 8, 11, 33, 24 };
+
+        //Array.Sort(nums);
+
+        //foreach(var item in nums)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        //int[, ,] arr = new int[2,2,3]{
+
+        //    { {1, 2, 3}, {3, 4, 5 } },
+        //   { {2, 3, 9}, {5, 6, 11} },
+
+        //};
+        //Console.WriteLine(arr.Rank);
+
+        // Array.Resize(ref numbers, 20);
+        //// numbers[10] = 40;
+
+        // foreach (var item in numbers)
+        // {
+        //     Console.WriteLine(item);
+        // }
+
+        //string name1 = "Eshgin";
+        //string surname1 = "Ceferli";
+        //int age1 = 21;
+
+        //string nema2 = "Ramil";
+        //string surname2 = "Binnetov";
+        //int age2 = 20;
+
+        //string name3 = "Azer";
+        //string surname3 = "Humbetov";
+        //int age3 = 21;
+
+        //Console.WriteLine(name1+ " "+surname1);
+
+        //var objNameSurNameAge = new
+        //{
+        //    name = "Eshgin",
+        //    surname = "Ceferli",
+        //    age = 22
+        //};
+
+
+        //var objNameSurNameAgee = new
+        //{
+
+        //    name = "Ramil",
+        //    surname = "Binnetov",
+        //    age = 20
+        //};            
+
+        ////Console.WriteLine(obj1.name + " " + obj1.surname);
+
+        //Console.WriteLine($"{obj2.name} {obj2.surname} Age: {obj2.age}");
+
+
+
+        //Student st1 = new Student(77,"Ehmedli");
+
+        //st1.name = "Eshgin";
+        //st1.surname = "Ceferli";
+        //st1.age = 22;
+
+        //Student st2 = new Student();
+        //st2.name = "Ramil";
+        //st2.surname = "Binnetov";
+        //st2.age = 20;
+
+
+        //Console.WriteLine(st1.FulLName( "Eshgin"));
+
+        //Console.WriteLine(st2.FullData());
+
+        //Student st3 = new Student
+        //{
+
+        //    name = "Cavid",
+        //    surname = "Bashirov",
+        //    age = 28
+        //};
+
+        //Student student = new Student();
+
+        //student.name = "Aziz";
+
+        //var result = student.GetGroupInfo();
+
+
+        //Console.WriteLine(result);
+
+    }
+
+
+}
+}
+
